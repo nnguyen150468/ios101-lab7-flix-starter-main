17b8e714de9b13807d4b48261401032b8507af7f
Nguyen Nguyen <n.dinhnguyen95@gmail.com>
2024-04-03 09:26:32 -0400

    Initial Commit

---
+++ ios101-lab7-flix/Movie.swift
@@ -0,0 +1,100 @@
+//
+//  Movie.swift
+//  ios101-lab6-flix
+//
+
+import Foundation
+
+struct MovieFeed: Decodable {
+    let results: [Movie]
+}
+
+struct Movie: Codable, Equatable {
+    let title: String
+    let overview: String
+    let posterPath: String? // Path used to create a URL to fetch the poster image
+
+    // MARK: Additional properties for detail view
+    let backdropPath: String? // Path used to create a URL to fetch the backdrop image
+    let voteAverage: Double?
+    let releaseDate: Date?
+
+    // MARK: ID property to use when saving movie
+    let id: Int
+
+    // MARK: Custom coding keys
+    // Allows us to map the property keys returned from the API that use underscores (i.e. `poster_path`)
+    // to a more "swifty" lowerCamelCase naming (i.e. `posterPath` and `backdropPath`)
+    enum CodingKeys: String, CodingKey {
+        case title
+        case overview
+        case posterPath = "poster_path"
+        case backdropPath = "backdrop_path"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+        case id
+    }
+}
+
+extension Movie {
+    static var favoritesKey: String {
+        return "Favorites"
+    }
+    
+    static func save(_ movies: [Movie], forKey key: String) {
+        // 1.
+        let defaults = UserDefaults.standard
+        // 2.
+        let encodedData = try! JSONEncoder().encode(movies)
+        // 3.
+        defaults.set(encodedData, forKey: key)
+    }
+    
+    static func getMovies(forKey key: String) -> [Movie] {
+        // 1.
+        let defaults = UserDefaults.standard
+        // 2.
+        if let data = defaults.data(forKey: key) {
+            // 3.
+            let decodedMovies = try! JSONDecoder().decode([Movie].self, from: data)
+            // 4.
+            return decodedMovies
+        } else {
+            // 5.
+            return []
+        }
+    }
+    
+    // Adds the movie to the favorites array in UserDefaults.
+    // 1. Get all favorite movies from UserDefaults
+    //    - We make `favoriteMovies` a `var` so we'll be able to modify it when adding another movie
+    // 2. Add the movie to the favorite movies array
+    //   - Since this method is available on "instances" of a movie, we can reference the movie this method is being called on using `self`.
+    // 3. Save the updated favorite movies array
+    func addToFavorites() {
+        // 1.
+        var favoriteMovies = Movie.getMovies(forKey: Movie.favoritesKey)
+        // 2.
+        favoriteMovies.append(self)
+        // 3.
+        Movie.save(favoriteMovies, forKey: Movie.favoritesKey)
+    }
+    
+    // Removes the movie from the favorites array in UserDefaults
+    // 1. Get all favorite movies from UserDefaults
+    // 2. remove all movies from the array that match the movie instance this method is being called on (i.e. `self`)
+    //   - The `removeAll` method iterates through each movie in the array and passes the movie into a closure where it can be used to determine if it should be removed from the array.
+    // 3. If a given movie passed into the closure is equal to `self` (i.e. the movie calling the method) we want to remove it. Returning a `Bool` of `true` removes the given movie.
+    // 4. Save the updated favorite movies array.
+    func removeFromFavorites() {
+        // 1.
+        var favoriteMovies = Movie.getMovies(forKey: Movie.favoritesKey)
+        // 2.
+        favoriteMovies.removeAll { movie in
+            // 3.
+            return self == movie
+        }
+        // 4.
+        Movie.save(favoriteMovies, forKey: Movie.favoritesKey)
+    }
+}
